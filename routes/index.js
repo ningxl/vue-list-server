80862f16a385700c5c71a0a1bad13011204e01d2
nxl <1033211644@qq.com>
2019-11-19 01:55:51 +0800

    update

---
+++ routes/index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+var User = require('../models/users');
+
+
+router.get('/getData', function (req, res) {
+  User.find({}, function (err, data) {
+    if (err) throw err;
+    res.send(data)
+  });
+});
+router.post('/setData', function (req, res) {
+  User.remove({}, function (err, data) {
+    if (err) throw err;
+    res.send(data);
+  });
+
+  for (var key in req.body) {
+    var user = new User((req.body[key]));
+    user.save(function (err, res) {
+      if (err) {
+        console.log(err);
+      } else {
+        
+      }
+    });
+  }
+
+});
+module.exports = router;
